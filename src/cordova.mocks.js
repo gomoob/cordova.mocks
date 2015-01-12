802681eb422323cc57cd5646ff5fcfe57848c7ec
Baptiste Gaillard <baptiste.gaillard@gomoob.com>
2015-01-12 14:28:52 +0100

    Add a mock for the Facebook connect Cordova plugin.

--- src/cordova.mocks.js
+++ src/cordova.mocks.js
@@ -1,3 +1,4 @@
 // @include cordova.js
+// @include plugins/com.phonegap.plugins.facebookconnect/www/js/facebookConnectPlugin.js
 // @include plugins/nl.x-services.plugins.socialsharing/www/SocialSharing.js
 // @include plugins/org.apache.cordova.splashscreen/www/splashscreen.js
