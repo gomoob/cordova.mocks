59579dd65c9a9b75472435a4a96101ffa5a7aa9b
Baptiste Gaillard <baptiste.gaillard@gomoob.com>
2015-05-13 18:48:47 +0200

    Document the cordova Events Simulation mode. Now the cordova events simulation mode has to be enabled.

--- README.md
+++ README.md
@@ -27,3 +27,30 @@ inside your `bower.json` file.
 
 ## Configuration
 
+## Cordova Events Simulation mode
+
+The library allows to enable a "Device Buttons simulation mode", this allows you to simulate Cordova events directly 
+inside your browser (see https://cordova.apache.org/docs/en/4.0.0/cordova_events_events.md.html). 
+
+To enable it open a console and enters `cordova.setSimulateDeviceButtons(true)`.
+
+```
+cordova.setSimulateDeviceButtons(true);
+cordova.mocks.js:46 Cordova mocks - device buttons simulation mode on.
+cordova.mocks.js:47 Be careful, the following keys are mapped to actions : 
+cordova.mocks.js:48  - Space key : Simulate a Cordova "pause" / "resume" event.
+cordova.mocks.js:49  - "+"   key : Simulate a Cordova "volumeupbutton" event.
+cordova.mocks.js:50  - "-"   key : Simulate a Cordova "volumedownbutton" event.
+cordova.mocks.js:51  - "B"   key : Simulate a Cordova "backbutton" event.
+cordova.mocks.js:52  - "C"   key : Simulate a Cordova "startcallbutton" / "endcallbutton" event.
+cordova.mocks.js:53  - "M"   key : Simulate a Cordova "menubutton" / "menubutton" event.
+cordova.mocks.js:54  - "S"   key : Simulate a Cordova "menubutton" / "searchbutton" event.
+```
+
+To disable it open a console and enters `cordova.setSimulateDeviceButtons(false)`. 
+
+```
+cordova.setSimulateDeviceButtons(false);
+Cordova mocks - device buttons simulation mode off.
+Cordova mocks - you can now used the keyboard normally again.
+```
--- src/cordova.js
+++ src/cordova.js
@@ -8,7 +8,7 @@ window.cordova.exec = function (success, fail, className, methodName, paras) {
     }
 };
 
-// A configuration object which is dedicated to the 'cordova.mocks' library
+//A configuration object which is dedicated to the 'cordova.mocks' library
 window.cordova.__mocks = {
 
     /**
@@ -23,7 +23,42 @@ window.cordova.__mocks = {
      *
      * @var {Boolean}
      */
-    PAUSED : false
+    PAUSED : false,
+
+    /**
+     * A boolean used to indicate if the device buttons simulation mode is enabled.
+     */
+    SIMULATE_DEVICE_BUTTONS : false
+
+};
+
+/**
+ * Enables or disabled the device buttons simulation mode.
+ *
+ * @param {Boolean} simulateDeviceButtons True to enable the device buttons simulation mode, false otherwise.
+ */
+window.cordova.setSimulateDeviceButtons = function(simulateDeviceButtons) {
+
+    window.cordova.__mocks.SIMULATE_DEVICE_BUTTONS = simulateDeviceButtons;
+
+    if(simulateDeviceButtons) {
+
+        console.log('Cordova mocks - device buttons simulation mode on.');
+        console.log('Be careful, the following keys are mapped to actions : ');
+        console.log(' - Space key : Simulate a Cordova "pause" / "resume" event.');
+        console.log(' - "+"   key : Simulate a Cordova "volumeupbutton" event.');
+        console.log(' - "-"   key : Simulate a Cordova "volumedownbutton" event.');
+        console.log(' - "B"   key : Simulate a Cordova "backbutton" event.');
+        console.log(' - "C"   key : Simulate a Cordova "startcallbutton" / "endcallbutton" event.');
+        console.log(' - "M"   key : Simulate a Cordova "menubutton" / "menubutton" event.');
+        console.log(' - "S"   key : Simulate a Cordova "menubutton" / "searchbutton" event.');
+
+    } else {
+
+        console.log('Cordova mocks - device buttons simulation mode off.');
+        console.log('Cordova mocks - you can now used the keyboard normally again.');
+
+    }
 
 };
 
@@ -33,102 +68,106 @@ document.onkeypress = function(event) {
 
     event = event || window.event;
 
-    var code = null;
+    if(window.cordova.__mocks.SIMULATE_DEVICE_BUTTONS) {
 
-    // Internet Explorer
-    if(event.which === null) {
+        var code = null;
 
-        code = event.keyCode;
+        // Internet Explorer
+        if(event.which === null) {
+
+            code = event.keyCode;
+
+        }
+
+        // Other browsers
+        else if (event.which !== 0 && event.charCode !== 0) {
+
+            code = event.which;
+
+        }
+
+        // Special keys
+        else {
+
+            return null;
+
+        }
+
+        switch(code) {
+
+            // Space key, Cordova 'pause' / 'resume' events
+            case 32:
+
+                // If the application is in pause mode we resume it
+                if(window.cordova.__mocks.PAUSED) {
+
+                    document.dispatchEvent(new Event('resume'));
+                    window.cordova.__mocks.PAUSED = false;
+
+                }
+
+                // Otherwise if the application is in resume mode we pause it
+                else {
+
+                    document.dispatchEvent(new Event('pause'));
+                    window.cordova.__mocks.PAUSED = true;
+
+                }
+
+                break;
+
+            // + Key, Cordova 'volumeupbutton' event
+            case 43:
+                document.dispatchEvent(new Event('volumeupbutton'));
+                break;
+
+             // - Key, Cordova 'volumedownbutton' event
+            case 45:
+                document.dispatchEvent(new Event('volumedownbutton'));
+                break;
+
+            // B Key, Cordova 'backbutton' event
+            case 98:
+                document.dispatchEvent(new Event('backbutton'));
+                break;
+
+            // C Key, Cordova 'startcallbutton' event
+            case 99:
+
+                // If the application is in calling mode we resume it
+                if(window.cordova.__mocks.CALLING) {
+
+                    document.dispatchEvent(new Event('endcallbutton'));
+                    window.cordova.__mocks.CALLING = false;
+
+                }
+
+                // Otherwise if the application is not in calling mode we place it in calling mode
+                else {
+
+                    document.dispatchEvent(new Event('startcallbutton'));
+                    window.cordova.__mocks.CALLING = true;
+
+                }
+
+                break;
+
+            // M Key, Cordova 'backbutton' event
+            case 109:
+                document.dispatchEvent(new Event('menubutton'));
+                break;
+
+            // S Key, Cordova 'backbutton' event
+            case 115:
+                document.dispatchEvent(new Event('searchbutton'));
+                break;
+
+            default:
+                return true;
+        }
+
+        return false;
 
     }
-
-    // Other browsers
-    else if (event.which !== 0 && event.charCode !== 0) {
-
-        code = event.which;
-
-    }
-
-    // Special keys
-    else {
-
-        return null;
-
-    }
-
-    switch(code) {
-
-        // Space key, Cordova 'pause' / 'resume' events
-        case 32:
-
-            // If the application is in pause mode we resume it
-            if(window.cordova.__mocks.PAUSED) {
-
-                document.dispatchEvent(new Event('resume'));
-                window.cordova.__mocks.PAUSED = false;
-
-            }
-
-            // Otherwise if the application is in resume mode we pause it
-            else {
-
-                document.dispatchEvent(new Event('pause'));
-                window.cordova.__mocks.PAUSED = true;
-
-            }
-
-            break;
-
-        // + Key, Cordova 'volumeupbutton' event
-        case 43:
-            document.dispatchEvent(new Event('volumeupbutton'));
-            break;
-
-         // - Key, Cordova 'volumedownbutton' event
-        case 45:
-            document.dispatchEvent(new Event('volumedownbutton'));
-            break;
-
-        // B Key, Cordova 'backbutton' event
-        case 98:
-            document.dispatchEvent(new Event('backbutton'));
-            break;
-
-        // C Key, Cordova 'startcallbutton' event
-        case 99:
-
-            // If the application is in calling mode we resume it
-            if(window.cordova.__mocks.CALLING) {
-
-                document.dispatchEvent(new Event('endcallbutton'));
-                window.cordova.__mocks.CALLING = false;
-
-            }
-
-            // Otherwise if the application is not in calling mode we place it in calling mode
-            else {
-
-                document.dispatchEvent(new Event('startcallbutton'));
-                window.cordova.__mocks.CALLING = true;
-
-            }
-
-            break;
-
-        // M Key, Cordova 'backbutton' event
-        case 109:
-            document.dispatchEvent(new Event('menubutton'));
-            break;
-
-        // S Key, Cordova 'backbutton' event
-        case 115:
-            document.dispatchEvent(new Event('searchbutton'));
-            break;
-
-        default:
-            break;
-    }
-
-    return false;
 
 };
